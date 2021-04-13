367d614e5f5988b4bb34576b9b0c163ab7a0d193
Andrey <reiforse@gmail.com>
2021-04-13 09:42:48 +0700

    improve readablity, remove implicit casting

--- lab9/main.c
+++ lab9/main.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 
 #define SUCCESS 0
+#define CHILD_PROCESS 0
 #define FAILURE 1
 #define ERROR -1
 
@@ -13,26 +14,26 @@ int main(int argc, char *argv[])
     pid_t pid;
 
     pid = fork();
-    if (pid > 0)
+    if (pid == ERROR)
     {
-        int waitResult = wait(0);
+        perror("lab9.out: fork the process");
+        exit(FAILURE);
+    }
+    else if (pid == CHILD_PROCESS)
+    {
+        execl("/bin/cat", "cat", "TheEmpireStrikesBackScript", (char *) 0);
+        perror("lab9.out: exec the programm");
+        exit(FAILURE);
+    }
+    else if (pid != CHILD_PROCESS)
+    {
+        int waitResult = wait(NULL);
         if (waitResult == ERROR)
         {
             perror("lab9.out: waiting for child process");
             exit(FAILURE);
         }
         printf("No, I'm your father\n");
-    }
-    else if (pid == 0)
-    {
-        execl("/bin/cat", "cat", "TheEmpireStrikesBackScript", (char *) 0);
-        perror("lab9.out: exec the programm");
-        exit(FAILURE);
-    }
-    else
-    {
-        perror("lab9.out: fork the process");
-        exit(FAILURE);
     }
     exit(SUCCESS);
 }
